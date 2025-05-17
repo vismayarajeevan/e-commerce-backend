c3fb303739a45153167ef1e532ce06d526781b1d
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-05-17 15:33:06 +0530

    auth model modified

--- model/authModel.js
+++ model/authModel.js
@@ -1,46 +1,4 @@
-// const mongoose = require('mongoose')
 
-// const userSchema = new mongoose.Schema({
-//     userName:{
-//         type:String,
-//         required:true
-//     },
-//     email:{
-//         type:String,
-//         required:true,
-//         unique:true
-//     },
-//     password:{
-//         type:String,
-//         required:true,
-//     },
-//     otp:{
-//         type:String
-//     },
-//     verified:{
-//         type:Boolean,
-//         default:false
-//     },
-//     isAdmin:{
-//         type:Boolean,
-//         default:false
-//     },
-    
-//     wishlist: [{
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'Product'
-//     }]
-    
-
-// },
-// {
-//     timestamps: true, 
-// }
-
-// )
-
-// const users = mongoose.model("user",userSchema)
-// module.exports = users
 
 const mongoose = require('mongoose');
 
